63f6928a58dc18bb3f2e99774b1e6c39115668b4
YecidMorenoUSP <yecidmoreno@usp.br>
2023-04-25 01:39:06 -0300

    back4 v0.0

--- back4/back4.cpp
+++ back4/back4.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <stdio.h>
-#include <list>
+#include <stack>
 
 using namespace std;
 
 enum{
-    _NULL = 0,
+    _NULL = -1,
     _VISITED = 1,
     _BEGIN = 2,
 };
@@ -16,7 +16,12 @@ enum{
 #define S1 "\x1B[32m\u25A0 "
 #define S3 "\x1B[35m\u25A0 "
 
-#define SZ 5
+#define iterate_M for(int i = 0 ; i < SZ ; i++) \
+                  for(int j = 0 ; j < SZ ; j++)\
+
+#define set_M(A) iterate_M *M_ptr(point(i,j)) = A;
+
+#define SZ 8
 
 typedef struct point
 {
@@ -31,8 +36,11 @@ point operator + (point & A , point & B ){
     return A;
 }
 
+bool operator == (point A , point B ){
+    return (A.x==B.x) && (A.y==B.y);
+}
+
 int M[SZ][SZ] = {0};
-list<point> q;
 
 void M_set(point p,int v){
     M[p.x][p.y] = v;
@@ -54,19 +62,26 @@ point directions[8] = {point( 1, 2),
 void printM(point p = point(-1,-1)){
     printf("\n");
     for(int i = 0 ; i < SZ ; i++){
-        for(int j = 0 ; j < SZ ; j++){
-            // printf("%2s",(M[i][j])?S1:S0);
-
-            
-                if(p.x == i && p.y == j){
-                    printf("%2s",S3);
-                }else{
-                    printf("%2s",(M[i][j])?S1:S0);
-                    // printf("\x1B[37m%2d",(M[i][j]));
-                }            
+        printf("    ");
+        for(int number = 0 ; number < 2 ; number++){
+            for(int j = 0 ; j < SZ ; j++){     
+                    if(p.x == i && p.y == j){
+                        if(number)
+                            printf("\x1B[35m%4d",(M[i][j]));
+                        else
+                            printf("%2s",S3);
+                    }else{
+                        if(number)
+                            printf("\x1B[37m%4d",(M[i][j]));
+                        else
+                            printf("%2s",(M[i][j]>0)?S1:S0);
+                    }            
+            }
+            printf("  |  ");
         }
         printf("\n");
     }
+    fflush(stdout);
 }
 
 void setM(point p,int v){
@@ -77,55 +92,67 @@ point movePoint(point p,int n){
     return (p+directions[n]);
 }
 
+bool isVisited(point p){
+    return (M[p.x][p.y]>=_VISITED);
+}
+
 bool isOK(point p){
-    return (p.x>=0) && (p.y>=0) && (p.x<SZ) && (p.y<SZ) && (M[p.x][p.y]==_NULL); 
+    return (p.x>=0) && (p.y>=0) && (p.x<SZ) && (p.y<SZ); 
 }   
 
-void printPoint(point p){
-    printf("[%2d,%2d]",p.x,p.y);
+void printPoint(point p, char * txt = (char *)""){
+    printf("[%2d,%2d]%s",p.x,p.y,txt);
 }
 
-void findPath(point p, int count = 0){
 
-    static int solves = 0;
-
-    if(count == (SZ*SZ-1)){
-        printf("count: %d\n",++solves); 
-        printM(p);
-        return;   
+bool isNextMoves(point p1, point p2){
+    for(int i = 0 ; i < 8 ; i++){
+        if(p2 == movePoint(p1,i) ){
+            return true;
+        }
     }
+    return false;
+}
+
+void findPath(point p, stack<point> &_path, int count = 1){
+    
+    point p_;
+    static int solves = 0;
+    static point p0;
+
+    if(count == (SZ*SZ)){       
+        if(isNextMoves(p,p0)){
+            printf("Solution found!!! : %d\n",++solves); 
+            _path.push(p);
+        }        
+    }    
+
     if(solves) return;
        
-    point p_;
+    if(count == 1) p0 = p;
+
+    *M_ptr(p) = _VISITED;   
 
     for(int i = 0 ; i < 8 ; i++){
-        
-        *M_ptr(p) = count+1;
-
         p_ = movePoint(p,i);
         
-        if(isOK(p_)){ 
+        if(isOK(p_)&&!isVisited(p_)){ 
             
+            findPath(p_,_path,count+1);
+
+            if(solves){
+                _path.push(p);
+                break;
+            }
             
-            *M_ptr(p) = _VISITED;
-
-            findPath(p_,count+1);
-
-            #if VERBOSE >= 2
-                if(solves == 1){
-                    system("sleep 0.1");
-                    system("clear");
-                    printM(p);
-                }
-            #endif
         }
 
-        *M_ptr(p) = _NULL;
-                  
     }
 
-
+    *M_ptr(p) = _NULL;
+    
 }
+
 
 
 typedef struct vertex
@@ -135,38 +162,125 @@ typedef struct vertex
 }vertex;
 
 
-int h(){
+int h(point p){
     static int count;
-
+    static point p_;
     count = 0;
-    for(int i = 0 ; i < SZ ; i++)
-    for(int j = 0 ; j < SZ ; j++)
-        if(M[i][j]!=0) count++;
+   for(int i = 0 ; i < 8 ; i++){
+        p_ = movePoint(p,i);
+        if(isOK(p_) && !isVisited(p_)){
+            count ++;
+        }
+   }
 
-    return (SZ*SZ)-count;
+    return count;
 }
 
-void findPathA(point p, int count = 0){
-
-    *M_ptr(p) = _VISITED;
+void findPathA(point p,stack<point> &_path, int count = 1){
+    point p_, p_next, p0 , p_max;
+    int i;
     
+    p0 = p;
+    p_next = p;
+
+    for(i = 0 ; i < SZ*SZ ; i++){
+
+        *M_ptr(p_next) = _VISITED;
+        _path.push(p_next);
+
+        int min_h = SZ+1, aux_h;
+        int s = rand()%SZ;
+
+        for(int j = 0 ; j < SZ ; j++){
+            p_ = movePoint(p_next,(j+s)%SZ);
+
+            if(isOK(p_) && !isVisited(p_)){
+                aux_h=h(p_);
+                if(aux_h<=min_h){
+                    min_h = aux_h;
+                    p_next = p_;
+                    p_max = p_;
+                }
+            }
+        }
+
+        if(min_h == ( SZ+1)) {
+            break;
+        }     
+
+    }
+
+    if(i == SZ*SZ-1 && isNextMoves(p_next,p0)){
+        printf("Solution found!!!");
+    }else{
+        _path = stack<point>();
+    }
+    
+}
+
+void animate(stack<point>  _path, bool sec = true, bool clear = false){
+    static point p;
+    static int count = 0;
+    set_M(_NULL);
+    count = 0;
+    
+    while (!_path.empty())
+    {           
+        p = _path.top();
+        *M_ptr(p) = ++count;        
+        
+        if(sec){
+            system("sleep .1");
+            if(clear)system("clear");
+            printM(p);    
+        }        
+
+        _path.pop();
+    }          
+    if(!sec)printM(p);
 }
 
 int main(){
+    
+    srand(time(NULL));
+
+    stack<point> _path0;
+    stack<point> _path1;
 
     system("clear");
-  
+    point p0(1,1);
+    
+    int xr,yr;
 
-    for(int i = 0 ; i < SZ ; i++)
-    for(int j = 0 ; j < SZ ; j++)
-    *M_ptr(point(i,j)) = _NULL;
+    while(true){
+        xr = rand(),yr = rand();
+        p0 = point((xr)%SZ,(yr)%SZ);
+        
+        system("clear");
+        printf("Finding solution for :");
+        printPoint(p0,(char *)"\n");       
 
-    point p0(2,2);
+        _path1 = stack<point>();
+        set_M(_NULL);
+        findPathA(p0,_path1); 
+        if(_path1.size()!=0){
+            break;
+        }
+    }
+    
+    // p0 = point(1,1);
+    
+    set_M(_NULL);
+    findPath(p0,_path0); 
+        
+    #if VERBOSE >= 2
 
-    findPath(p0);  
+        animate(_path0,false,false);
 
+        animate(_path1,false,false); 
+         
+    #endif
 
     
-
     return 0;
 }
