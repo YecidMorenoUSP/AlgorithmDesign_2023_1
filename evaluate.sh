3102090a79b57ce88a6a5d19a1c8bde768bb5351
YecidMorenoUSP <yecidmoreno@usp.br>
2023-06-10 13:29:47 -0300

    add stdout

--- evaluate.sh
+++ evaluate.sh
@@ -1,5 +1,7 @@
 
 NAME="$1"
+N="$2"
+COUT="$3"
 
 cd $NAME
 
@@ -8,34 +10,44 @@ CPP_FILE="*.cpp"
 for f in $CPP_FILE
 do
     echo "Compiling $f file..."
-    g++ $f -o a.out
+    g++ $f -o a.out -Wall
 done
 
-FILES="*.in"
+if [ "$N" = "" ]; then
+    FILES="*.in"
+else
+    FILES="$N.in"
+fi
+
+echo $FILES
 
 for f in $FILES
 do
     n=${f%.*}
     echo ""
 
-    ./a.out < $n.in > out.out
+    time ./a.out < $n.in > out.out
 
     res="FAIL"
-    if cmp -s $n.out out.out; then
+    if cmp -s "$n.out" "out.out"; then
         res="OK"
     fi
 
     echo "----------$n.in-------------[$res]"
         
-    cat out.out
-    cat $n.out
+    if [ "$COUT" != "" ]; then
+        cat out.out
+        cat $n.out
+    fi
+    # cat out.out
+    # cat $n.out
 
     if [ "$res" != "OK" ]; then
         echo " "
         # diff $n.out out.out
     fi
 
-    rm out.out
+    # rm out.out
 
 done
 
